586b2c562621a01f181a13abbad517a82d136b55
vladbelk <vladbelk321@gmail.com>
2023-12-11 12:42:21 +0200

    hm ex7

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 try:
-    print("nothing")
-    print("will work")
-    print("unless you do")
+    print("Довжина сторін прямокутника")
+    a = float(input("a = "))
+    b = float(input("b = "))
+    print("Площа: %.2f"% (a*b))
 
 except Exception as e:
     print(e)

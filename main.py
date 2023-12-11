5ae6a3cd0fcf3af78ee5fc13d9f0f50da322324e
vladbelk <vladbelk321@gmail.com>
2023-12-11 12:00:23 +0200

    hm ex5

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 try:
-    print('\033[33m"Anyone who\033[33m')
-    print("stops")
-    print("learning is old,")
-    print('\033[33mwhether at twenty or eighty"\033[0m')
-    print("Henry Ford")
+    print("nothing")
+    print("will work")
+    print("unless you do")
 
 except Exception as e:
     print(e)

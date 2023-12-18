f9c124fef626431d5fe7ac98b15c55deb3b4df58
vladbelk <vladbelk321@gmail.com>
2023-12-18 19:36:50 +0200

    hm17.12№1

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ try:
     a = int(input("Перше число = "))
     b = int(input("Друге число = "))
     s = 0
-    for i in range(a,b+1):
-        s=s+1
+    for i in range(a, b+1):
+        s = s + a
 
-    print(f"Сума двух чисел = {a+b}")
-    print(f"Середнє арифметичне = {s/2} ")
-
+    print(f"Сума чисел в діапазоні = {s}")
+    print(f"Середнє арифметичне двух = {(a + b) / 2} ")
+    print(f"Середнє арифметичне всіх = {s / (b - a)} ")
 
 
 except Exception as e:
